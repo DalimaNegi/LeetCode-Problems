6c50f2f8ee1ebca1caa48236874a936eefc23b98
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-11 22:56:59 +0530

    Time: 405 ms (96.79%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -4,3 +4,4 @@ SELECT MAX(num) as num
 FROM (SELECT num from MyNumbers GROUP BY num HAVING COUNT(num) = 1) as unique_Numbers;
 
 # Data filtered -> directly accessed from "FROM" .
+# Subquery executed from "FROM unique_numbers".
