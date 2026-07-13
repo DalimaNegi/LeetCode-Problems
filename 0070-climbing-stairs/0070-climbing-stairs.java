ecdc8fad6228d1c674ebf6bb0f78cf7834d75864
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-13 21:45:45 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,12 +2,12 @@ class Solution {
     public int climbStairs(int n) {
         int prev = 1;
         int curr = 1;
-        for(int i = 2; i<=n; i++){
+        for(int i = 2; i<=n; i++){      //Complexity - O(n)
             int temp = curr;
             curr = prev + curr;
             prev = temp;
         }
-        return curr;
+        return curr;    //stores the fibbonacci term
     }
 
     // series -> 1 2 3 5 8....
