516e00cba484e37a8afa3e4d1edd2f2cf4c2d3dd
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-13 21:44:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.63%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev = 1;
+        int curr = 1;
+        for(int i = 2; i<=n; i++){
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+
+    // series -> 1 2 3 5 8....
+    // n = 3 gives 3
+
+    // fibonacci approach
+    // if(n<=2){
+    //         return n;
+    //     }
+    // return climbStairs(n-1) + climbStairs(n-2);   Complexity - O(2^n)
+}
