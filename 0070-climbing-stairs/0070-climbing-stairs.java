50c4bb36b7ef591360226386057d4875bd05ea20
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-16 21:46:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,24 @@
 class Solution {
-    public int climbStairs(int n) {
-        int prev = 1;
-        int curr = 1;
-        for(int i = 2; i<=n; i++){      //Complexity - O(n)
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+    public int fib(int n, int storedValue[]){
+        if(n <= 2){
+            return n;
         }
-        return curr;    //stores the fibbonacci term
+        if(storedValue[n] != -1){
+            return storedValue[n];
+        }
+
+        storedValue[n] = fib(n-1,storedValue) + fib(n-2, storedValue);
+        return storedValue[n];
+    }
+    public int climbStairs(int n) {
+        // memoization -> to store values of frequent computations. -> O(n)
+        
+        int[] storedValue = new int[n+1];
+        for(int i =1; i<=n; i++){
+            storedValue[i] = -1;
+        }
+        int result = fib(n, storedValue);
+        return result;
     }
 
     // series -> 1 2 3 5 8....
@@ -18,4 +29,16 @@ class Solution {
     //         return n;
     //     }
     // return climbStairs(n-1) + climbStairs(n-2);   Complexity - O(2^n)
+
+
+    // public int climbStairs(int n) {
+    //     int prev = 1;
+    //     int curr = 1;
+    //     for(int i = 2; i<=n; i++){      //Complexity - O(n)
+    //         int temp = curr;
+    //         curr = prev + curr;
+    //         prev = temp;
+    //     }
+    //     return curr;    //stores the fibbonacci term
+    // }
 }
