aa302effebb04b7e60d1f9c9e5bd966f57acfcda
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-17 23:57:53 +0530

    Time: 526 ms (99.26%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,17 @@
 # Write your MySQL query statement below
 
+with realData as(
+    SELECT id
+    FROM (
+        SELECT email , MIN(id) as id
+        FROM Person
+        GROUP BY email
+    ) as data
+)
 
-DELETE p1 FROM Person p1 JOIN Person p2           # Cartesian Product CROSS JOIN
-WHERE p1.email = p2.email and p1.id > p2.id;  
+DELETE FROM PERSON
+WHERE id not in (SELECT * FROM realData);
+
+# Way - 1
+-- DELETE p1 FROM Person p1 JOIN Person p2           # Cartesian Product CROSS JOIN
+-- WHERE p1.email = p2.email and p1.id > p2.id;  
