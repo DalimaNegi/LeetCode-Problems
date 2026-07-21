bef955d4160c1a7fe8180f2702c6488ea323cf81
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-21 21:12:00 +0530

    Time: 0 ms (100%), Space: 9.2 MB (69.27%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -5,32 +5,50 @@
  *     struct ListNode *next;
  * };
  */
- #include <math.h>
-struct ListNode* findElementsFromMid(int mid,struct ListNode* head){
-    int i=1;
-    while(i<mid){
-        head = head -> next;
-        i++;
+
+// WAY - 1
+
+struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
     }
-    return head;
+
+    return slow;
 }
 
-struct ListNode* middleNode(struct ListNode* head) {
-    int length=0;
-    struct ListNode* temp = head;
-    while(temp!=NULL){
-        length++;
-        temp = temp->next;
-    }
-    struct ListNode* final;
-    int mid;
-    if(length%2==0){
-        mid = (length/2)+1;
-        final = findElementsFromMid(mid,head);
-    }else{
-        mid = ceil(length/2.0);
-        final = findElementsFromMid(mid,head);
-    }
-    return final;    
-    //we only return the address of the third node as all the rest of the nodes are linked to it.
-}
+
+// WAY -2
+
+//  #include <math.h>
+// struct ListNode* findElementsFromMid(int mid,struct ListNode* head){
+//     int i=1;
+//     while(i<mid){
+//         head = head -> next;
+//         i++;
+//     }
+//     return head;
+// }
+
+// struct ListNode* middleNode(struct ListNode* head) {
+//     int length=0;
+//     struct ListNode* temp = head;
+//     while(temp!=NULL){
+//         length++;
+//         temp = temp->next;
+//     }
+//     struct ListNode* final;
+//     int mid;
+//     if(length%2==0){
+//         mid = (length/2)+1;
+//         final = findElementsFromMid(mid,head);
+//     }else{
+//         mid = ceil(length/2.0);
+//         final = findElementsFromMid(mid,head);
+//     }
+//     return final;    
+//     //we only return the address of the third node as all the rest of the nodes are linked to it.
+// }
