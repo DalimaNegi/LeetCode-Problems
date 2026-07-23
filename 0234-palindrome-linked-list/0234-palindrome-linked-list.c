bc57aeaba5fe94ab1c344e5ceabb36b714c2f880
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-23 13:38:09 +0530

    Time: 0 ms (100%), Space: 45.1 MB (99.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.c
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -5,35 +5,77 @@
  *     struct ListNode *next;
  * };
  */
-bool isPalindrome(struct ListNode* head) {          //Time & space cmplx -> O(n) as there is an extra array
-    struct ListNode * temp = head;
-    int count = 0;
-    while(temp!=NULL){
-        count++;
-        temp = temp->next;
+
+struct ListNode * reverse (struct ListNode * head){
+    struct ListNode * current = head;
+    struct ListNode * prev = NULL;
+    struct ListNode * temp;
+    while(current != NULL){
+        temp = current->next;
+        current -> next = prev;
+        prev = current;
+        current = temp;
     }
-    int arr[count];
-    int i = 0;
-    
-    struct ListNode * temp1 = head;
-    while(temp1 != NULL && i<count){
-        arr[i]= temp1 -> val;
-        i++;
-        temp1 = temp1->next;
+    head = prev;
+    return head;
+}
+
+bool isPalindrome(struct ListNode* head){
+    struct ListNode * slow = head;
+    struct ListNode * fast = head;
+
+    while(fast != NULL && fast->next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
     }
 
-    int j = count-1;
-    int k = 0;
-    while(k<j){
-        if(arr[k] == arr[j]){
-            k++;
-            j--;
-        }else{
+    if(fast != NULL){
+        slow = slow -> next;
+    }
+
+    struct ListNode * start = head;
+    struct ListNode * secondHalf = reverse(slow);
+    while(secondHalf!= NULL){
+        if(start->val != secondHalf->val){
             return false;
         }
+        start = start -> next;
+        secondHalf = secondHalf -> next;
     }
     return true;
 }
+
+
+// bool isPalindrome(struct ListNode* head) {          
+//     //Time & space cmplx -> O(n) as there is an extra array
+//     struct ListNode * temp = head;
+//     int count = 0;
+//     while(temp!=NULL){
+//         count++;
+//         temp = temp->next;
+//     }
+//     int arr[count];
+//     int i = 0;
+    
+//     struct ListNode * temp1 = head;
+//     while(temp1 != NULL && i<count){
+//         arr[i]= temp1 -> val;
+//         i++;
+//         temp1 = temp1->next;
+//     }
+
+//     int j = count-1;
+//     int k = 0;
+//     while(k<j){
+//         if(arr[k] == arr[j]){
+//             k++;
+//             j--;
+//         }else{
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 
 //Below approach works only on some test cases because it ends up reversing the original linked list.
