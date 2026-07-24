b32fbd57e958b304126505137aacc712acd0a7c7
DalimaNegi <157436841+DalimaNegi@users.noreply.github.com>
2026-07-24 18:15:52 +0530

    Time: 496 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
 
-SELECT (
-    SELECT DISTINCT salary FROM Employee ORDER BY salary desc LIMIT 1 OFFSET 1
-) as SecondHighestSalary;
+-- SELECT (
+--     SELECT DISTINCT salary FROM Employee ORDER BY salary desc LIMIT 1 OFFSET 1
+-- ) as SecondHighestSalary;    # Time and Space cmplx - O(n)
 
 -- LIMIT - no. of rows to extract
 -- OFFSET - row position (0-n)
+
+SELECT MAX(salary) as SecondHighestSalary 
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee); # Time -O(n) and Space cmplx- O(1)
